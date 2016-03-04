dfc425adacb7b86b9d20a1715a21634a2bddf879
sasha <e-finder@rambler.ru>
2016-03-04 21:14:04 +0300

    commit to local rep
    it's good if i cope with it

---
+++ next one/js/slide.js
@@ -0,0 +1,37 @@
+﻿$('document').ready(function(){
+	var itemsPerPage=3;
+	var waresLine=$('.wares_line');
+	waresLine.each(function(){
+		var itemsCount=$(this).find('.wares_item').length;
+		var stagesCount=Math.ceil(itemsCount/itemsPerPage);
+		$(this).find('.count').html(stagesCount);
+
+	});
+
+	$('.control').click(function(){
+
+		var item=$(this).siblings().children('.wares_lent').children('.wares_item');
+		var itemWidth=parseInt(item.css('width').replace('px',""));
+		var margin=parseInt(item.css('margin-right').replace('px',""));
+		var offset=(itemWidth+margin)*itemsPerPage;
+		var stage=$(this).siblings('.indication').children('.stage');
+		var waresLent=$(this).siblings('.frame').children('.wares_lent')
+		if ($(this).hasClass('prev')){
+			if (parseInt(stage.html())>1){
+				waresLent.animate({"left":"+="+offset+'px'}, 'slow');
+				stage.html(parseInt(stage.html())-1);
+			}
+
+		}
+		else{
+
+			if (parseInt(stage.html())<parseInt($(this).siblings('.indication').children('.count').html())){
+				waresLent.animate({"left":"-="+offset+'px'}, 'slow');
+				stage.html(parseInt(stage.html())+1);
+			}
+		}
+		
+	});
+
+});
+
