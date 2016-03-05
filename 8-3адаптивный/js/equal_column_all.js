3c9861b6e54a2c7ed9d789005a4d12ec82eb8461
sasha <e-finder@rambler.ru>
2016-03-05 14:33:35 +0300

    commiting rest projects
    yes

---
+++ 8-3адаптивный/js/equal_column_all.js
@@ -0,0 +1,34 @@
+﻿
+jQuery('document').ready(function(){
+
+	var a=jQuery('.column');
+	var maxh=a.height();
+	a.each(function(){
+		
+
+		if ($(this).height()>maxh){
+			maxh=$(this).height();
+
+		}
+	});
+
+    a.each(function(){
+    	if($(this).css('float')!='none'){
+    		$(this).height(maxh);
+    	}
+
+
+    });
+	
+
+
+
+
+
+	
+
+    
+
+
+
+});
